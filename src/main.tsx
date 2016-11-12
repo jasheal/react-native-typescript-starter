bf95686e69df20fcbf65b59c85b7315b4b88644b
Jason Healy <hello@speakstud.io>
2016-11-12 20:56:40 +0000

    Initial setup to convert to .tsx

---
+++ src/main.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { Component } from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-native";
+
+class App extends Component<any, any> {
+
+  public render(): JSX.Element {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+            Welcome to React Native TypeScript
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit src/main.tsx
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{"\n"}
+          Cmd+D or shake for options
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  } as React.ViewStyle,
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  } as React.TextStyle,
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5,
+  } as React.TextStyle,
+});
+
+AppRegistry.registerComponent("ReactNativeTS", () => App);
