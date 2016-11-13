d4b5d2d2d816da3bb54693dace6e87337f338542
Jason Healy <hello@speakstud.io>
2016-11-13 17:44:38 +0000

    update module resolution

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import React, {Component} from "react";
+import * as React from "react";
+import { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 interface Props {}
