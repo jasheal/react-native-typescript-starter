0f22c642bde3903f559b2abea3251481632c4751
Jason Healy <jhealy@jasheal.com>
2017-02-06 11:11:29 +0000

    Update dependencies and XCODE Project settings

--- type-declarations/modules.d.ts
+++ type-declarations/modules.d.ts
@@ -1,19 +1,24 @@
-// The following modules do not have type declarations available. We can add declarations in here as we need them.
+/* 
+    Application Module Definitions
+    Author: Jason Healy 
+    ---------------------------------------------------------------------------
+    The following modules do not have type declarations available. 
+    Definitions need to be added in here as required.
+
+    Adding definitions
+    ---------------------------------------------------------------------------
+*/ 
 
 declare module "react-native-navigation" {
     export var Navigation: any;
 }
 
-declare module "redux-persist/constants" {
-    export const REHYDRATE: any;
-}
-
-declare module "redux-persist" {
-    export var persistStore: any;
-    export var purgeStoredState: any;
-}
-
 declare module "remote-redux-devtools" {
     var devTools: any;
     export default devTools;
+}
+
+declare module "react-native-vector-icons/Ionicons" {
+    var Icon: any;
+    export default Icon;
 }
